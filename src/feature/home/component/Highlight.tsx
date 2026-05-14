e4bdb12e3336cf544b20d7109d2690695f71f236
emingcloud <emingcloud@gmail.com>
2026-05-14 16:51:11 +0700

    fix highlight

--- src/feature/home/component/Highlight.tsx
+++ src/feature/home/component/Highlight.tsx
@@ -63,17 +63,17 @@ export default function Highlight() {
             </p>
 
             {/* Action Buttons Placeholder */}
-            <div
-              onClick={() =>
-                navigate(`/watch/${movie.id}`, {
-                  state: {
-                    video: movie.video,
-                  },
-                })
-              }
-              className="flex gap-4 pt-2"
-            >
-              <button className="bg-white hover:cursor-pointer text-black px-6 py-2 rounded font-semibold hover:bg-white/90 transition">
+            <div className="flex gap-4 pt-2">
+              <button
+                onClick={() =>
+                  navigate(`/watch/${movie.id}`, {
+                    state: {
+                      video: movie.video,
+                    },
+                  })
+                }
+                className="bg-white hover:cursor-pointer text-black px-6 py-2 rounded font-semibold hover:bg-white/90 transition"
+              >
                 Play
               </button>
               <button
